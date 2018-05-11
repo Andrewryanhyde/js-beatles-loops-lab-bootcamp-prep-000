9d614425ae640c0bdcd4ff63aa5c3db5a12fcedd
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-11 11:51:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function theBeatlesPlay(musicians, instruments) {
   let a = [] 
   for (let i = 0; i < musicians.length; i++) {
-    let b = "${musicians} + ' plays ' + ${instruments}"
+    let b = ${musicians} + ' plays ' + ${instruments}
+    b.toString
     a.push(b)
   }
   return a
