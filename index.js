1269c18e3f70aadb5d051157ce60b25a34b8723f
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-11 12:25:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ function theBeatlesPlay(musicians, instruments) {
 
 function johnLennonFacts(facts) {
   
+  let newFacts = []
+  
   for(i = 0; i < facts.length; i++) {
-    facts[i].unshift
+    
   }
   return facts;
 }
